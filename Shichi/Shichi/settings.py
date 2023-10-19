faa705002d745125d4aa568f7d11132436b15ab2
soltaniali <soltanibookali1380@gmail.com>
2023-10-19 19:27:22 +0330

    chore: change the db and add all deps

--- Shichi/Shichi/settings.py
+++ Shichi/Shichi/settings.py
@@ -112,8 +112,12 @@ WSGI_APPLICATION = 'Shichi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shichidb', 
+        'USER': 'postgres',    
+        'PASSWORD': '1234',  
+        'HOST': 'localhost', 
+        'PORT': '',          
     }
 }
 
