d43659801725b3a242adb73aa258d318f970ddbf
soltaniali <soltanibookali1380@gmail.com>
2023-10-19 22:01:16 +0330

    refactor: add .env file for constants

--- Shichi/Shichi/settings.py
+++ Shichi/Shichi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+from environs import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+# Environment Variables
+env = Env()
+env.read_env()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&5jm4puv0doo$(@-z&of@nb9*x%kwbe=)!6a3ra!k-tbgbvqkg'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=True)
 
 AUTH_USER_MODEL = 'CustomUser.CustomUser'
 
@@ -113,11 +118,11 @@ WSGI_APPLICATION = 'Shichi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'shichidb', 
-        'USER': 'postgres',    
-        'PASSWORD': '1234',  
-        'HOST': 'localhost', 
-        'PORT': '',          
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),    
+        'PASSWORD': env.str('DB_PASSWORD'),  
+        'HOST': env.str('DB_HOST'), 
+        'PORT': '5432',          
     }
 }
 
