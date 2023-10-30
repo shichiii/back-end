bf61c7bfbcada6fadc52d721c2736e77d9fb86ad
reyhane <reyhane.shahrokh@gmail.com>
2023-10-30 12:54:14 +0330

    fix: A minor change in AdvertisementCRUD

--- Shichi/CustomAdvertisement/views.py
+++ Shichi/CustomAdvertisement/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, filters
 from .models import CustomAdvertisement
 from .serializers import customAdvertisementCreateSerializer, customAdvertisementSerializer
 from rest_framework.response import Response
