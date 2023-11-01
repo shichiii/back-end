3154cf28d34b702fac4586e7fd4f6fa6bc31cb75
reyhane <reyhane.shahrokh@gmail.com>
2023-11-01 14:32:54 +0330

    feat: car image crud

---
+++ Shichi/CustomCarImage/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import CustomCarImage
+
+class CustomCarImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomCarImage
+        fields = '__all__'
