19234fbf7f83e9ba17279a543d3117e0fa7a9a72
unknown <frnz.azad2002@gmail.com>
2023-11-01 15:11:23 +0330

    fix: add first_name and last_name for login_signup

--- Shichi/CustomUser/serializers.py
+++ Shichi/CustomUser/serializers.py
@@ -7,7 +7,7 @@ from django.contrib.auth.hashers import make_password, check_password
 class LoginSignupCustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'email', 'password',]
+        fields = ['id', 'email', 'password', 'first_name', 'last_name',]
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
