a8fd966e457dd5fd074397db5bb791e60b7d40ea
reyhane <reyhane.shahrokh@gmail.com>
2023-11-01 18:04:53 +0330

    fix: url for advertisement fixed

--- Shichi/Shichi/urls.py
+++ Shichi/Shichi/urls.py
@@ -23,4 +23,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('CustomUser.urls')),
+    path('advertisement/', include('CustomeAdvertisement.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
