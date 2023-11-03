df832ce4b9cdc578c5baf467b424645a98e928de
helen azad <74285390+helenAzad@users.noreply.github.com>
2023-11-03 21:11:39 +0330

    feat: add email for forgotpass

--- Shichi/Shichi/settings.py
+++ Shichi/Shichi/settings.py
@@ -242,7 +242,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shichiiii777@gmail.com'
-EMAIL_HOST_PASSWORD = 'eightminusone'
-DEFAULT_FROM_EMAIL = 'shichiiii777@gmail.com'
-SERVER_EMAIL = 'eightminusone'
+EMAIL_HOST_USER = 'troyteam22@gmail.com'
+
+EMAIL_HOST_PASSWORD ='yvcpvkazuuqnvguy'
+DEFAULT_FROM_EMAIL = 'troyteam22@gmail.com'
+SERVER_EMAIL = 'troyteam22@gmail.com'
