3042a0bd6a8152bd25e3bb6fe79e9cc48f7a0041
soltaniali <soltanibookali1380@gmail.com>
2023-11-03 19:03:26 +0330

    feat: multiple history added

--- Shichi/CustomHistories/views.py
+++ Shichi/CustomHistories/views.py
@@ -1,7 +1,24 @@
+from rest_framework.views import APIView
 from rest_framework import viewsets
 from .models import CustomHistories
-from .serializers import CustomHistoriesSerializer
+from CustomAdvertisement.models import CustomAdvertisement
+from .serializers import CustomHistoriesSerializer , CustomHistoriesSerializerBackDoor
+from rest_framework.response import Response
+from rest_framework import status
 
-class CustomHistoriesViewSet(viewsets.ModelViewSet):
+class CustomHistoriesViewSetBackdoor(viewsets.ModelViewSet):
+    queryset = CustomHistories.objects.select_related('advertisement').all()
+    serializer_class = CustomHistoriesSerializerBackDoor
+    
+class CustomHistoriesViewSet(APIView):
     queryset = CustomHistories.objects.select_related('advertisement').all()
     serializer_class = CustomHistoriesSerializer
+
+    def get(self, request):
+        queryset = CustomHistories.objects.filter(advertisement__owner_id=request.user.id)
+        
+        if queryset.exists():
+            all_serializers = CustomHistoriesSerializer(queryset, many=True)
+            return Response(all_serializers.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response({'error': 'No related history found.'}, status=status.HTTP_404_NOT_FOUND)
