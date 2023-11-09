054759db7b50f552955669b292f16c5d5a3937d8
reyhane <reyhane.shahrokh@gmail.com>
2023-11-09 15:06:34 +0330

    fix: model updated

--- Shichi/CustomHistories/models.py
+++ Shichi/CustomHistories/models.py
@@ -4,4 +4,4 @@ from CustomAdvertisement.models import CustomAdvertisement
 class CustomHistories(models.Model):
     advertisement = models.ForeignKey(CustomAdvertisement, on_delete=models.DO_NOTHING)
     created_date = models.DateTimeField(auto_now_add=True)
-    
+    take_or_own = models.CharField(default="take", blank=False)
