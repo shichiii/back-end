a0c3913907f95fcee53e780cb2d5753f58975848
reyhane <reyhane.shahrokh@gmail.com>
2023-11-23 22:33:10 +0330

    fix: other things fixed according to changing the model

--- Shichi/CustomAdvertisement/serializers.py
+++ Shichi/CustomAdvertisement/serializers.py
@@ -1,24 +1,22 @@
 from rest_framework import serializers
 from .models import CustomAdvertisement, Comment, Rate
-from CustomAdvertisementLocation.serializers import CustomAdvertisementLocationSerializer
 from CustomCarImage.serializers import CustomCarImageSerializer
 from django.db.models import Avg
 
 class customAdvertisementCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomAdvertisement
-        fields = ['id', 'owner_id', 'location', 'start_date', 'end_date', 'price', 'description', 'car_images', 'car_name', 'car_color', 'car_produced_date','car_seat_count','car_door_count', 'car_Is_cooler', 'car_gearbox', 'car_fuel', 'car_category', 'car_image2']
+        fields = ['id', 'owner_id', 'start_date', 'end_date', 'price', 'description', 'car_images', 'car_name', 'car_color', 'car_produced_date','car_seat_count','car_door_count', 'car_Is_cooler', 'car_gearbox', 'car_fuel', 'car_category', 'location_state', 'location_geo_width', 'location_geo_lenght']
         read_only_fields = ['id', 'owner_id', 'created_date']
 
 
 class customAdvertisementSerializer(serializers.ModelSerializer):
-    location = CustomAdvertisementLocationSerializer(read_only=True)
     car_images = CustomCarImageSerializer(read_only=True, many=True)
     average_rating = serializers.SerializerMethodField()
 
     class Meta:
         model = CustomAdvertisement
-        fields = ['id', 'average_rating', 'owner_id', 'location', 'start_date', 'end_date', 'price', 'description', 'car_images', 'car_name', 'car_color', 'car_produced_date', 'car_seat_count','car_door_count', 'car_Is_cooler', 'car_gearbox', 'car_fuel', 'car_category', 'car_image2']
+        fields = ['id', 'average_rating', 'owner_id', 'start_date', 'end_date', 'price', 'description', 'car_images', 'car_name', 'car_color', 'car_produced_date', 'car_seat_count','car_door_count', 'car_Is_cooler', 'car_gearbox', 'car_fuel', 'car_category', 'location_state', 'location_geo_width', 'location_geo_lenght']
     
     def get_average_rating(self , obj):
         print(obj.id)
--- Shichi/CustomAdvertisementLocation/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import CustomAdvertisementLocation
-
-class CustomAdvertisementLocationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomAdvertisementLocation
-        fields = '__all__'
