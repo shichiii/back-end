7942059b6119bbe0adb04a52cf163accda9a20ed
soltaniali <soltanibookali1380@gmail.com>
2023-12-29 04:00:00 +0330

    feat: add the image and full name in chat rooms

--- Shichi/Chat/serializers.py
+++ Shichi/Chat/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import ChatRoom , Message
+from CustomUser.models import CustomUser
 
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id' , 'first_name' , 'last_name' , 'profile_image' ]  # Include the fields you want in the response
 class ChatRoomSerializers(serializers.ModelSerializer):
+    sender = CustomUserSerializer()
+    reciver = CustomUserSerializer()
     class Meta:
         model = ChatRoom
         fields = '__all__'
@@ -10,3 +17,5 @@ class MessageSerializers(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = '__all__'
+
+
